e9a24e9b95d625f82f0d416ccdd446d491418e51
Anas Ardah <anas.m.ardah@gmail.com>
2025-01-05 21:41:06 +0300

    testing

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail', // Use your email service
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    pass: process.env.EMAIL_PASSWORD,
   },
 });
 
