77d3e5022855b15f31b7da92ab4b27ed65b2b2a6
Anas Ardah <anas.m.ardah@gmail.com>
2025-01-10 15:51:42 +0300

    1.1.0

--- index.js
+++ index.js
@@ -1,42 +1,28 @@
 require('dotenv').config();
 const express = require('express');
-const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const mailgun = require('mailgun-js');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Configure CORS
-app.use(cors({
-  origin: 'https://technology-master.netlify.app', // Allow requests from your React app's domain
-}));
-
 app.use(bodyParser.json());
 
-// Configure Nodemailer
-const transporter = nodemailer.createTransport({
-  service: 'gmail', // Use your email service
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD,
-  },
-});
+const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN});
 
-// Contact form endpoint
 app.post('/api/contact', (req, res) => {
   const { name, email, phone, message } = req.body;
 
-  const mailOptions = {
+  const data = {
     from: email,
     to: process.env.EMAIL_USER,
     subject: 'New Contact Form Submission',
     text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`,
   };
 
-  transporter.sendMail(mailOptions, (error, info) => {
+  mg.messages().send(data, (error, body) => {
     if (error) {
-      console.error('Error sending email:', error);
+      console.error(error);
       return res.status(500).json({ error: 'Failed to send email' });
     }
     res.status(200).json({ message: 'Email sent successfully' });
