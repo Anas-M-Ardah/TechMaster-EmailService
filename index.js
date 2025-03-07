07e57d718a77630a0d5abc243fe3e6896cfb6eaf
Anas Ardah <anas.m.ardah@gmail.com>
2025-03-07 11:28:21 +0300

    testing 2

--- index.js
+++ index.js
@@ -104,6 +104,7 @@ const validateContact = [
 
   (req, res, next) => {
     const errors = validationResult(req);
+    console.log(errors);
     if (!errors.isEmpty()) {
       return res.status(400).json({
         success: false,
